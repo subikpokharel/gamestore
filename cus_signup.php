7b2ac140d6b42a502edfbd2ff61e5586ef8696c0
Subik Pokharel <subikpokharel93@gmail.com>
2017-10-03 00:08:20 -0500

    Pages Login and SignUp

---
+++ cus_signup.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Gamestore | Customer Signup</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="public/bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="public/template.min.css">
+  </head>
+  <body class="hold-transition register-page">
+    <div class="register-box">
+      <div class="register-logo">
+        <a href=""><b>Customer</b>Signup</a>
+      </div>
+
+      <div class="register-box-body">
+        <p class="login-box-msg">Register a new Customer</p>
+        <form action="#" method="post">
+          <div class="form-group has-feedback">
+            <input type="text" class="form-control" placeholder="Full name">
+          </div>
+          <div class="form-group has-feedback">
+            <input type="email" class="form-control" placeholder="Email">
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="Password">
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="Retype password">
+          </div>
+          <div class="row">
+            <div class="col-xs-1"></div><!-- /.col -->
+            <div class="col-xs-5">
+              <button type="register" class="btn btn-primary btn-block btn-flat">Register</button>
+            </div><!-- /.col -->
+            <div class="col-xs-1"> </div><!-- /.col -->
+            <div class="col-xs-5">
+              <button type="reset" class="btn btn-danger btn-block btn-flat">Reset</button>
+            </div><!-- /.col -->
+          </div>
+        </form><br>
+        <a href="index.php" class="text-center">Back to Login page</a>
+      </div><!-- /.form-box -->
+    </div><!-- /.register-box -->
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Gamestore | Log in</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="public/bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="public/template.min.css">
+
+  </head>
+  <body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href=""><b>Game</b>store</a>
+      </div><!-- /.login-logo -->
+      <div class="login-box-body">
+        <p class="login-box-msg">Sign in to start your session</p>
+        <form action="#" method="post">
+          <div class="form-group has-feedback">
+            <input type="email" class="form-control" placeholder="Email">
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="Password">
+          </div>
+          <div class="row">
+            <div class="col-xs-2">
+            </div><!-- /.col -->
+            <div class="col-xs-8">
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            </div><!-- /.col -->
+            <div class="col-xs-2">
+            </div><!-- /.col -->
+          </div>
+        </form><br>
+        <a href="cus_signup.php" class="text-center">New Customer? Sign Up!!</a> <br>
+        <a href="dev_signup.php" class="text-center">Developer SignUp</a>
+
+      </div><!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+
+  </body>
+</html>
